54a23e7c297bd4bc900a46ea642a9969bb98ca5a
Diego <diego.ariza@mercadolibre.com.co>
2022-09-27 17:51:25 -0500

    creacion de un Bean con parametros

---
+++ src/main/java/com/hagakure/fundamentos/bean/MyOperation.java
@@ -0,0 +1,5 @@
+package com.hagakure.fundamentos.bean;
+
+public interface MyOperation {
+    int sum(int  numberA, int numberB);
+}
---
+++ src/main/java/com/hagakure/fundamentos/bean/MyOperationImplement.java
@@ -0,0 +1,8 @@
+package com.hagakure.fundamentos.bean;
+
+public class MyOperationImplement implements MyOperation{
+    @Override
+    public int sum(int numberA, int numberB) {
+        return numberA + numberB;
+    }
+}
